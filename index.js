b626eca2220c3e65ec10c5d13558e27ebc57011d
Elijah Silverman <elijahmsilverman@gmail.com>
2021-10-14 10:56:11 -0400

    commit tree 23ee5fd3e1e80ea8d65ed7442f939ba946da6f6f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    const newDrivers = drivers.filter(word => word.toUpperCase() === string.toUpperCase())
+        return newDrivers
+}
+
+function fuzzyMatch(drivers, string) {
+    const newDrivers = drivers.filter(word => word.startsWith(string))
+        return newDrivers
+}
+
+function matchName(drivers, string) {
+    const newArr = drivers.filter(curItem => curItem.name === string)
+        return newArr
+}
